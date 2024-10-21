1b8668c8dc6e8d2beea5a80a81eb64c08c3e3f89
Andy <github-aguzmant103@donotrackplus.com>
2024-10-21 11:53:04 -0600

    Initial Commit

---
+++ ring-battleTests/ring_battleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ring_battleTests.swift
+//  ring-battleTests
+//
+//  Created by Andres Guzman  on 21/10/2024.
+//
+
+import Testing
+@testable import ring_battle
+
+struct ring_battleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
